139367913418ad4184014bc7bc339c170eb68ebd
mohammedwaleed321 <mhmdalbfry848@gmail.com>
2025-11-21 21:51:50 +0200

    this program is very useful for some people
    this program made by c++ , its made also for people dont know the multiplication table

---
+++ multiplication_table.cpp
@@ -0,0 +1,28 @@
+/*
+multiplication table
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    //make variable for number
+    int num;
+    cout << "please enter number: ";
+    cin >> num;
+    //print multiplication table
+    cout << "multiplication table of " << num << " is: \n";
+    cout << num << "*1= " << num * 1 << "\n";
+    cout << num << "*2= " << num * 2 << "\n";
+    cout << num << "*3= " << num * 3 << "\n";
+    cout << num << "*4= " << num * 4 << "\n";
+    cout << num << "*5= " << num * 5 << "\n";
+    cout << num << "*6= " << num * 6 << "\n";
+    cout << num << "*7= " << num * 7 << "\n";
+    cout << num << "*8= " << num * 8 << "\n";
+    cout << num << "*9= " << num * 9 << "\n";
+    cout << num << "*10= " << num * 10 << "\n";
+    cout << num << "*11= " << num * 11 << "\n";
+    cout << num << "*12= " << num * 12 << "\n";
+    return 0;
+
+}
+//program ended
